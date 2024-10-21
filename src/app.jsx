48e83bb5accbc94ec5b8db5a63492797da250dc4
Mauricio Neto <mauricioazvedo@gmail.com>
2024-10-21 00:01:23 -0300

    Feat: Iniciando instalação do ionic.

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,20 @@
 import ComponentHeader from "./components/header/componentHeader";
 import ComponentSideNav from "./components/sideNav/componentSideNav";
+import '@ionic/react/css/core.css';
+import { setupIonicReact } from '@ionic/react';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 import { Outlet } from "react-router-dom";
+
+setupIonicReact();
 
 const App = () => {
   return (
