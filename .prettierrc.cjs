18218239cb9ceeeebee7190e249baeeceedd79f4
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-11-15 15:23:37 -0500

    Add login form component and linter settings

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  arrowParens: 'avoid'
+}
--- src/pages/Login/pageLogin.jsx
+++ src/pages/Login/pageLogin.jsx
@@ -1,46 +1,37 @@
-import React, { useState } from 'react';
-import { IonInput, IonButton, IonItem, IonLabel } from '@ionic/react';
-import AuthService from '../../services/authService'; 
+import React, { useState } from 'react'
+import { IonInput, IonButton, IonItem, IonLabel } from '@ionic/react'
+import AuthService from '../../services/authService'
 
 const PageLogin = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null); // Adicionando o estado de erro
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null) // Adicionando o estado de erro
 
   const handleLogin = async () => {
     try {
-      const token = await AuthService.login(email, password);
-      console.log('Login bem-sucedido!', token);
+      const token = await AuthService.login(email, password)
+      console.log('Login bem-sucedido!', token)
     } catch (error) {
-      setError(error, 'Falha ao fazer login. Verifique suas credenciais.');
+      setError(error, 'Falha ao fazer login. Verifique suas credenciais.')
     }
-  };
+  }
 
   return (
     <div>
       <IonItem>
         <IonLabel position="floating">Email</IonLabel>
-        <IonInput
-          type="email"
-          value={email}
-          onIonChange={(e) => setEmail(e.detail.value)}
-        />
+        <IonInput type="email" value={email} onIonChange={e => setEmail(e.detail.value)} />
       </IonItem>
-
       <IonItem>
         <IonLabel position="floating">Password</IonLabel>
-        <IonInput
-          type="password"
-          value={password}
-          onIonChange={(e) => setPassword(e.detail.value)}
-        />
+        <IonInput type="password" value={password} onIonChange={e => setPassword(e.detail.value)} />
       </IonItem>
-
       {error && <p style={{ color: 'red' }}>{error}</p>} {/* Exibindo mensagem de erro */}
-
-      <IonButton expand="block" onClick={handleLogin}>Login</IonButton>
+      <IonButton expand="block" onClick={handleLogin}>
+        Login
+      </IonButton>
     </div>
-  );
-};
+  )
+}
 
-export default PageLogin;
+export default PageLogin
